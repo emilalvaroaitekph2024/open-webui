e3b0ef7b5c41d83715466e3aa0f2dbbc35e4f712
Emil Alvaro AitekPH <ceo@aitekph.com>
2024-07-28 10:21:32 +0800

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,20 +3,25 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
-  kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: 'index.html'
-    })
-  },
-  onwarn: (warning, handler) => {
-    const { code } = warning;
-    if (code === 'css-unused-selector') return;
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html'
+		})
+	},
+	onwarn: (warning, handler) => {
+		const { code, _ } = warning;
+		if (code === 'css-unused-selector') return;
 
-    handler(warning);
-  }
+		handler(warning);
+	}
 };
 
 export default config;
