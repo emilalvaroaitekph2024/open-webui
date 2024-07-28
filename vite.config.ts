242a4094c7e1d7edaed7b927ced460bbbda6b727
Emil Alvaro AitekPH <ceo@aitekph.com>
2024-07-28 10:37:46 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -16,15 +16,16 @@ import { defineConfig } from 'vite';
 // };
 
 export default defineConfig({
-        plugins: [sveltekit()],
-        define: {
-                APP_VERSION: JSON.stringify(process.env.npm_package_version),
-                APP_BUILD_HASH: JSON.stringify(process.env.APP_BUILD_HASH || 'dev-build')
-        },
-        build: {
-                sourcemap: true
-        },
-        worker: {
-                format: 'es'
-        }
+  plugins: [sveltekit()],
+  define: {
+    APP_VERSION: JSON.stringify(process.env.npm_package_version),
+    APP_BUILD_HASH: JSON.stringify(process.env.APP_BUILD_HASH || 'dev-build')
+  },
+  build: {
+    sourcemap: true,
+    outDir: 'dist'
+  },
+  worker: {
+    format: 'es'
+  }
 });
